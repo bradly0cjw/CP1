4520294f74a28baa776bd7a4a95805f03612d253
bradly0cjw <bradly093@gmail.com>
2022-10-13 12:00:37 +0800

    NMSL

---
+++ w5.c
@@ -0,0 +1,10 @@
+int main(){
+    int size;
+    size=sizeof(int);
+    size=sizeof(char);
+    size=sizeof(1.1);
+    size=sizeof(size);
+    size=sizeof('A');
+    size=sizeof("NIMASILE");
+    return 0;
+}
