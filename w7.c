2b20703adcc186b3948cb0340cd9c6c1a7d016af
bradly0cjw <bradly093@gmail.com>
2022-11-01 09:54:18 +0800

    c++?

--- w7.c
+++ w7.c
@@ -3,5 +3,14 @@ int main(){
     int a;
     scanf("%d",&a);
     printf("%d is %s \n",a,a&1?"odd":"even");
+    
     return 0;
 }
+// int main() {
+//     int n, m;
+//     m = 5;
+//     n = 3;
+//     // (m > n ? m : n) = -1;
+//     // c++ usage
+//     printf("%d %d", m, n);
+// }
