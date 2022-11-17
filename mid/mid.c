6e62056948a4c05f37f4a24e80a3325967601535
bradly0cjw <bradly093@gmail.com>
2022-11-17 08:45:17 +0800

    midreview

---
+++ mid/mid.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+// int main(){
+//     int m=65535,max=0;
+//     while(m!=0){
+//         if(m%10>max){
+//             max=m%10;
+//         } 
+//         m=m/10;
+//     }
+//     return 0;
+// }
+
+// int main2(){
+//     int m1=1,m2=-10,m3=10,m4=1,max;
+//     max=m1;
+//     if (max<m2){
+//         max=m2;
+//     }
+//     if(max<m3){
+//         max=m3;
+//     }
+//     if(max<m4){
+//         max=m4;
+//     }
+//     return 0;
+// }
+
+// int main3(){
+//     int m=2,n=3,sum=0;
+//     for(int i=1;n;i++){
+//         sum=sum+m;
+//     } 
+//     return 0;
+// }
+
+// int main4(){
+//     int m=12321,count=0;
+//     while(m!=0){
+//         if(((m%10)%2)!=0){
+//             count=count+1;
+//         }
+//         m=m/10;
+//     }
+//     return 0;
+// }
+
+// int main5(){
+//     int m1=8,m2=3,m3=4,min,max,result;
+//     min=m1;
+//     max=m1;
+//     if (min>m2){
+//         min=m2;
+//     }
+//     if (min>m3){
+//         min=m3;
+//     }
+//     if (max<m2){
+//         max=m2;
+//     }
+//     if (max<m3){
+//         max=m3;
+//     }
+//     result=max-min;
+//     return 0;
+// }
+
+int main(){
+    int m=010;
+    printf("%d",m);
+    return 0;
+}
